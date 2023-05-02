571664d8137de717f9a3723addd6a62a331b2d34
SONG-YEE <119464641+SONG-YEE@users.noreply.github.com>
2023-05-02 16:17:37 +0900

    fst commt

---
+++ parseWithApi/src/main/java/com.assignment.parseWithApi/ParseWithApiApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.parseWithApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParseWithApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParseWithApiApplication.class, args);
+	}
+
+}
